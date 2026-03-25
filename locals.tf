a5c1784ba298eff34bdf435f98a199cc5af31648
srini <srinivaskaipu2705@gmail.com>
2026-03-25 16:38:38 -0400

    Initial commit : MOve module file to root

---
+++ locals.tf
@@ -0,0 +1,10 @@
+locals {
+  common_tags = {
+    Projrct = var.project_name
+    Environment = var.environment
+    Terraform = true
+  }
+
+  common_name_suffix = "${var.project_name}-${var.environment}"  # roboshop-dev
+  az_names = slice(data.aws_availability_zones.available.names, 0, 2 )
+}
